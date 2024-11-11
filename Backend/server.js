a7accc647389b553258b882e14c9b7074b3a6aaa
Rahul-Tejwani <148797374+Rahul-Tejwani@users.noreply.github.com>
2024-11-11 21:15:41 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,14 @@
 import app from "./app.js";
+import path from "path";
+import express from "express";
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on Port ${process.env.PORT}`);
+  console.log(`Server running on Port ${process.env.PORT}`);
 });
